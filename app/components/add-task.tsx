c51da8bdfad0e86aecbe0efca498a0f7b33d1152
Sobin <sobingeneral@gmail.com>
2023-10-08 18:35:56 +0700

    feat: add new task button

---
+++ app/components/add-task.tsx
@@ -0,0 +1,11 @@
+import { BiPlus } from "react-icons/bi";
+export default function AddTask() {
+  return (
+    <div>
+      <button className="btn btn-accent btn-md">
+        <BiPlus size={19} /> Add New Task
+      </button>
+      {/* <h1>this is a task2</h1> */}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-white w-screen h-screen">
       <div className="text-center text-black">
-        <h1 className="text-2xl pt-5">Todo List</h1>
+        <h1 className="text-2xl pt-5 pb-5">Todo List</h1>
         <AddTask />
       </div>
     </div>
