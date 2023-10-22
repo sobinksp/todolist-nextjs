6136d7b8018610719eb1b33d2ebf95e38fa265de
Sobin <sobingeneral@gmail.com>
2023-10-22 12:36:03 +0700

    feat: improve page appearance

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import getTasks from "./api/task";
 import AddTask from "./components/add-task";
+import TodoList from "./components/todo-list";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getTasks();
+  console.log(tasks);
   return (
-    <div className="bg-white w-screen h-screen">
-      <div className="text-center text-black">
-        <h1 className="text-2xl pt-5 pb-5">Todo List</h1>
+    <div className="bg-white w-screen h-screen text-center">
+      <div className="flex flex-col gap-4 text-black p-5 items-center">
+        <h1 className="text-2xl">Todo List</h1>
         <AddTask />
+        <TodoList tasks={tasks} />
       </div>
     </div>
   );
