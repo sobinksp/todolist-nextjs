3d7c23d3029af77ff207c6e3f46bd6d94e08d656
Sobin <sobingeneral@gmail.com>
2023-10-22 14:40:21 +0700

    feat: add new task api

--- app/api/task.ts
+++ app/api/task.ts
@@ -2,13 +2,20 @@ import { ITask } from "@/types/TaskInterface";
 
 const jsonServerUrl = "http://localhost:3004";
 
-export default async function getTasks(): Promise<ITask[]> {
-  const response = await fetch(`${jsonServerUrl}/tasks`);
-  const tasks = await response.json();
+export async function getTasks(): Promise<ITask[]> {
+  const res = await fetch(`${jsonServerUrl}/tasks`, { cache: "no-store" });
+  const tasks = await res.json();
   return tasks;
 }
-// export const getTasks = async (): Promise<ITask[]> => {
-//   const response = await fetch(`${jsonServerUrl}/tasks`);
-//   const tasks = await response.json();
-//   return tasks;
-// };
+
+export async function addNewTask(task: ITask): Promise<ITask[]> {
+  const res = await fetch(`${jsonServerUrl}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+  const newTask = await res.json();
+  return newTask;
+}
