e067a524a71d3a82e449e6f5e26cfa8768dd6dbc
Sobin <sobingeneral@gmail.com>
2023-10-22 12:35:39 +0700

    feat: add task display

---
+++ app/components/todo-list.tsx
@@ -0,0 +1,27 @@
+import { ITask } from "@/types/TaskInterface";
+import Task from "./task";
+
+// props are a type of object where the value of attribute of a tag is stored.
+// the word props implies properties
+interface TodoListProps {
+  tasks: ITask[];
+}
+export default function TodoList({ tasks }: TodoListProps) {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Tasks</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
