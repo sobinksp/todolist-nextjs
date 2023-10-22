029eaca2abf1130ea750753fa44708d2c8891aa0
Sobin <sobingeneral@gmail.com>
2023-10-22 14:41:05 +0700

    feat: change page width

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import getTasks from "./api/task";
+import { getTasks } from "./api/task";
 import AddTask from "./components/add-task";
 import TodoList from "./components/todo-list";
 
@@ -6,7 +6,7 @@ export default async function Home() {
   const tasks = await getTasks();
   console.log(tasks);
   return (
-    <div className="bg-white w-screen h-screen text-center">
+    <div className="max-w-6xl mx-auto text-center">
       <div className="flex flex-col gap-4 text-black p-5 items-center">
         <h1 className="text-2xl">Todo List</h1>
         <AddTask />
